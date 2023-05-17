4705708925633c2b1d1fd0679e98d0d057d1adb2
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-05-17 11:58:36 -0300

    AuthorDTO

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.AuthorDTO;
 import com.example.workshopmongo.repository.PostRepository;
 import com.example.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 	
-		Post post1 = new Post(null,sdf.parse("17/12/2021"),"Minha primeira  viagem","vou viajar pela primeira vez",maria);
-		Post post2 = new Post(null,sdf.parse("17/12/2022"),"Partiu viagem","vou viajar",maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null,sdf.parse("17/12/2021"),"Minha primeira  viagem","vou viajar pela primeira vez",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("17/12/2022"),"Partiu viagem","vou viajar",new AuthorDTO(maria));
+		
+		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
