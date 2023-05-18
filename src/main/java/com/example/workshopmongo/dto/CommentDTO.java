9a1f50b23871afb170a99f62c1ce5772fa453900
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-05-18 09:27:28 -0300

    Comments

---
+++ src/main/java/com/example/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,48 @@
+package com.example.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CommentDTO implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String text;
+	private Date date;
+	private AuthorDTO authordto;
+
+	public CommentDTO() {
+	}
+
+	public CommentDTO(String text, Date date, AuthorDTO authordto) {
+		this.text = text;
+		this.date = date;
+		this.authordto = authordto;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AuthorDTO getAuthordto() {
+		return authordto;
+	}
+
+	public void setAuthordto(AuthorDTO authordto) {
+		this.authordto = authordto;
+	}
+
+}
